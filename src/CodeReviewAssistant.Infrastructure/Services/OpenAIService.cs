eb44835b49f8f32de4b513d7823b1e75ca0afa94
sidprabaharan <21sssp1l@gmail.com>
2024-12-18 18:22:37 -0500

    Fix security vulnerability in API

--- src/CodeReviewAssistant.Infrastructure/Services/OpenAIService.cs
+++ src/CodeReviewAssistant.Infrastructure/Services/OpenAIService.cs
@@ -6,3 +6,5 @@ Initial content for src/CodeReviewAssistant.Infrastructure/Services/OpenAIServic
 // Modified at 2025-03-10T02:40:16.600936
 
 // Modified at 2025-03-10T02:40:22.324705
+
+// Modified at 2025-03-10T02:40:23.300860
