7dfb1946b83e39b3174d3205c9866a985f0f3218
sidprabaharan <21sssp1l@gmail.com>
2024-12-26 22:31:12 -0500

    Add statistics charts to dashboard

--- src/CodeReviewAssistant.Web/src/components/Dashboard.jsx
+++ src/CodeReviewAssistant.Web/src/components/Dashboard.jsx
@@ -4,3 +4,5 @@ Initial content for src/CodeReviewAssistant.Web/src/components/Dashboard.jsx
 // Modified at 2025-03-10T02:40:24.838147
 
 // Modified at 2025-03-10T02:40:25.479419
+
+// Modified at 2025-03-10T02:40:29.026919
