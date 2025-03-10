7108e814384607d7e3d4f3b9f0a2355a1e2dffa4
Sid Prabaharan <137012018+sidprabaharan@users.noreply.github.com>
2025-03-10 02:54:26 -0400

    Update OpenAIService.cs

--- src/CodeReviewAssistant.Infrastructure/Services/OpenAIService.cs
+++ src/CodeReviewAssistant.Infrastructure/Services/OpenAIService.cs
@@ -1,14 +1,1 @@
-# OpenAIService.cs
 
-Initial content for src/CodeReviewAssistant.Infrastructure/Services/OpenAIService.cs
-// Modified at 2025-03-10T02:40:15.599430
-
-// Modified at 2025-03-10T02:40:16.600936
-
-// Modified at 2025-03-10T02:40:22.324705
-
-// Modified at 2025-03-10T02:40:23.300860
-
-// Modified at 2025-03-10T02:40:29.343895
-
-// Modified at 2025-03-10T02:40:33.182554
